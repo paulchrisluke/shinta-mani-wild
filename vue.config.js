4c12bbb99f0ed6c68fe6a19f969c5b1a881bde28
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-08-12 18:49:18 +0430

    add global style, add shintamani font, cleanup hellow world

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
+const webpack = require("webpack");
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env": {
+          APP_VERSION:
+            '"' +
+            escape(JSON.stringify(require("./package.json").version)) +
+            '"',
+          BUILD_DATE: new Date().getTime()
+        }
+      })
+    ]
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "src/styles/global.scss";`
+      }
+    }
+  }
 };
