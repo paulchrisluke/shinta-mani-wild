08b2a28eb54c849630d11a918cdc3af2d3a5713a
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-08-22 14:39:14 +0430

    add swagger to axios package

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import '@/connection/init'
 
 Vue.config.productionTip = false
 Vue.prototype.gridBreakpoints = { xs: 0, sm: 576, md: 768, lg: 992, xl: 1200 }
