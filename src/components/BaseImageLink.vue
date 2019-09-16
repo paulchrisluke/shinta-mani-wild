57d072aab7d86c1ac4efa4e2c2a039936aec66b6
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-09-16 12:11:32 +0430

    separate button background to 3 part

--- src/components/BaseImageLink.vue
+++ src/components/BaseImageLink.vue
@@ -1,10 +1,16 @@
 <template>
   <a
     :class="className"
-    class="image-link d-inline-flex justify-content-center align-items-center button-frame font-serif text-uppercase"
+    class="image-link d-inline-flex justify-content-center align-items-center button-frame font-serif text-uppercase position-relative"
     :aria-label="text"
-    v-text="text"
-  ></a>
+  >
+    <div class="image-link--images-wrapper d-flex position-absolute">
+      <div class="image-link--left h-100"></div>
+      <div class="image-link--center h-100 flex-grow-1"></div>
+      <div class="image-link--right h-100"></div>
+    </div>
+    <span class="image-link--text" v-text="text"></span>
+  </a>
 </template>
 
 <script lang="ts">
@@ -30,8 +36,26 @@ export default Vue.extend({
   border: none;
   background-color: transparent;
   font-size: rem(32px);
+  .image-link--left {
+    width: rem(59px);
+  }
+  .image-link--right {
+    width: rem(50px);
+  }
   &.is-secondary {
-    background-image: url('https://res.cloudinary.com/ddwsbpkzk/image/upload/h_72/Shinta%20Mani%20Wild/general/Book_Now_Button_Light_jy1vtg.png');
+    // background-image: url('https://res.cloudinary.com/ddwsbpkzk/image/upload/h_72/Shinta%20Mani%20Wild/general/Book_Now_Button_Light_jy1vtg.png');
+    .image-link--left {
+      background: url('https://res.cloudinary.com/ddwsbpkzk/image/upload/h_72/Shinta%20Mani%20Wild/general/Button_Broken_Left_nyexsi.png')
+        no-repeat center;
+    }
+    .image-link--center {
+      background: url('https://res.cloudinary.com/ddwsbpkzk/image/upload/h_72/Shinta%20Mani%20Wild/general/Button_Broken_Mid_cu34ha.png')
+        repeat-x center;
+    }
+    .image-link--right {
+      background: url('https://res.cloudinary.com/ddwsbpkzk/image/upload/h_72/Shinta%20Mani%20Wild/general/Button_Broken_Right_hyntrd.png')
+        no-repeat center;
+    }
     color: $dark;
     &:hover {
       text-decoration: none;
@@ -39,7 +63,18 @@ export default Vue.extend({
     }
   }
   &.is-primary {
-    background-image: url('https://res.cloudinary.com/ddwsbpkzk/image/upload/h_72/Shinta%20Mani%20Wild/general/Book_Now_Button_Dark_nh7zuz.png');
+    .image-link--left {
+      background: url('https://res.cloudinary.com/ddwsbpkzk/image/upload/h_72/Shinta%20Mani%20Wild/general/Button_Broken_Left_nyexsi.png')
+        no-repeat center;
+    }
+    .image-link--center {
+      background: url('https://res.cloudinary.com/ddwsbpkzk/image/upload/h_72/Shinta%20Mani%20Wild/general/Button_Broken_Mid_cu34ha.png')
+        repeat-x center;
+    }
+    .image-link--right {
+      background: url('https://res.cloudinary.com/ddwsbpkzk/image/upload/h_72/Shinta%20Mani%20Wild/general/Button_Broken_Right_hyntrd.png')
+        no-repeat center;
+    }
     color: $white;
     &:hover {
       text-decoration: none;
@@ -47,11 +82,18 @@ export default Vue.extend({
     }
   }
   &.is-md {
-    $button-width: 173px;
     $button-height: 72px;
-    width: rem($button-width);
+    padding: 0 rem(24px);
     height: rem($button-height);
-    background-size: rem($button-width $button-height);
   }
+}
+.image-link--text {
+  z-index: 1;
+}
+.image-link--images-wrapper {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 }
 </style>
