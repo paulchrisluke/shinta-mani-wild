a1a0df96a7ff393d81f92ac7eb2429176872d0b4
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-09-20 18:29:13 +0430

    tents - update contents

--- src/views/Tents.vue
+++ src/views/Tents.vue
@@ -75,7 +75,7 @@
                     :type="'h2'"
                     :class-name="'h2 text-dark text-center text-xl-left'"
                   ></base-heading>
-                  <p>Shinta Mani Wild is a radical new fusion of world-class design, hospitality and conservation. The camp was born when world-renowned designer Bill Bensley identified an unprotected, 350-hectare wildlife corridor – and set out to save it from poaching, mining and logging. Sustainability informs everything we do, hence our partnership with the Wildlife Alliance (whose rangers have an onsite station), and our resident naturalists, who assist our chef. The Shinta Mani Fo on wildlife expeditions, foraging tours and more.</p>
+                  <p>Shinta Mani Wild is the full package - in every sense of the word. Our rates are inclusive of a round trip private car transfer to/from Phnom Penh or Sihanoukville Airport and vicinity, as well as your personal Bensley Butler, all meals and beverages, privately guided activities to explore the Cardamom forest, luxury spa treatments, WiFi throughout the camp and of course daily laundry service.</p>
                 </div>
               </template>
             </base-card>
@@ -218,7 +218,7 @@ export default Vue.extend({
       })
     },
     cardImage1(): object | undefined {
-      const image = 'http://placehold.it/500x400'
+      const image = 'https://res.cloudinary.com/ddwsbpkzk/image/upload/v1567747072/Shinta%20Mani%20Wild/Tents/All_Inclusive_-_Tent_Page_Shinta_Mani_Wild_kdfq4g.jpg'
       if (!image) {
         return
       }
@@ -233,7 +233,7 @@ export default Vue.extend({
       }
     },
     cardImage2(): object | undefined {
-      const image = 'http://placehold.it/500x400'
+      const image = 'https://res.cloudinary.com/ddwsbpkzk/image/upload/v1568948083/Shinta%20Mani%20Wild/Tents/Head_Butler_Mac_-_Shinta_Mani_Wild_xxc4i9.jpg'
       if (!image) {
         return
       }
