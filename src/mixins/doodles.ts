14fadb246467c2a79cdfda116637f45edc3adcdf
paulchrisluke <chris@whynot.earth>
2019-10-05 21:15:11 +0700

    Change background doodle animation
    increase speed a touch, reduce duration to 0

--- src/mixins/doodles.ts
+++ src/mixins/doodles.ts
@@ -30,7 +30,7 @@ export default {
       AOS.init({
         once: true,
         // NOTE: If you change duration, you should update lib-aos.scss
-        duration: 3000,
+        duration: 0,
         easing: 'ease-in-out',
         anchorPlacement: 'center-center'
       })
@@ -43,7 +43,7 @@ export default {
       })
     },
     getRellaxSpeed() {
-      return -0.1
+      return -1.5
     },
     relativeDoodleAmount(
       itemsLength: number,
