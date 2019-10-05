4f51749f51e2fe8f6430730c2ae96155d2cd5c87
paulchrisluke <chris@whynot.earth>
2019-10-05 20:54:10 +0700

    initial changes
    needs review. Basically, I'm trying to remove the fade travel distance initially. I just want them fading in without any position changes.
    
    On doodles.ts changes anchor placement to center-center to attempt to anchor it center (avoiding travel distance)
    
    on lib-aos I commented out fade distance, easing, and fading, trying to remove travel distance on fade in. I feel like this is probably the wrong way to go about it, and I'm messing up stuff I don't realize.
    
    The next step is to mess with the fixed position. I want the background images to stay in palce while the rest of the content is loading. Perhaps I need to play with the offset distance?

--- src/mixins/doodles.ts
+++ src/mixins/doodles.ts
@@ -32,7 +32,7 @@ export default {
         // NOTE: If you change duration, you should update lib-aos.scss
         duration: 3000,
         easing: 'ease-in-out',
-        anchorPlacement: 'center-bottom'
+        anchorPlacement: 'center-center'
       })
     },
     setItemParallax(event: any) {
